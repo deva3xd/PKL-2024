e9627e32db7f7fcaf18910c408fbc9a896ab8789
Raved <devarn05@gmail.com>
2024-03-05 10:37:30 +0700

    fix(ui): input

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,24 +10,24 @@
             </div>
         </div>
         <div class="w-1/2 flex justify-center items-center bg-white">
-            <div class="text-center text-black">
+            <div class="text-center text-black w-7/12">
                 <div class="text-4xl font-bold">
                     <h1>Buat akun</h1>
                 </div>
                 <form method="POST" action="{{ route('storeRegister') }}">
                     @csrf
-                    <div class="mt-8 flex items-center gap-4 border p-4 rounded-xl" style="background-color: #F1F1F1">
+                    <label class="mt-8 h-14 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                        <input type="text" placeholder="Username" class="focus:outline-none w-full" style="background-color: #F1F1F1" name="username" required />
-                    </div>
-                    <div class="mt-4 flex items-center gap-4 border p-4 rounded-xl" style="background-color: #F1F1F1">
+                        <input type="text" class="grow" placeholder="Username" name="username" required />
+                    </label>
+                    <label class="mt-4 h-14 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
-                        <input type="password" placeholder="Password" class="focus:outline-none w-full" style="background-color: #F1F1F1" name="password" required min="8" />
-                    </div>
-                    <div class="mt-4 flex items-center gap-4 border p-4 rounded-xl" style="background-color: #F1F1F1">
+                        <input type="password" class="grow" placeholder="Password" name="password" required />
+                    </label>
+                    <label class="mt-4 h-14 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
-                        <input type="password" placeholder="Konfirmasi Password" class="focus:outline-none w-full" style="background-color: #F1F1F1" name="password_confirmation" required min="8" />
-                    </div>
+                        <input type="password" class="grow" placeholder="Konfirmasi Password" name="password_confirmation" required />
+                    </label>
                     <button type="submit" class="btn border-none mt-8 p-4 rounded-xl text-white w-full bg-blue-700 hover:bg-blue-800 font-bold text-1xl">REGISTER</button>
                 </form>
                 <p>Sudah punya akun? <a href="{{ route('login') }}" class="text-blue-700">Masuk</a> Sekarang</p>
