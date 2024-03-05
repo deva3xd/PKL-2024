0a258aa25b86306ed12e5dc89bd6eaef0c652dc3
Raved <devarn05@gmail.com>
2024-03-05 11:23:41 +0700

    update ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                 </div>
                 <form method="POST" action="{{ route('postLogin') }}">
                     @csrf
-                    <label class="mt-8 h-14 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
+                    <label class="mt-5 h-14 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
                         <input type="text" class="grow" placeholder="Username" name="username" required />
                     </label>
@@ -24,14 +24,10 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path fill-rule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clip-rule="evenodd" /></svg>
                         <input type="password" class="grow" placeholder="Password" name="password" required />
                     </label>
-                    <button type="submit" class="btn border-none mt-8 p-4 rounded-xl text-white w-full bg-blue-700 hover:bg-blue-800 font-bold text-1xl">LOG IN</button>
+                    <button type="submit" class="btn border-none mt-5 p-4 rounded-xl text-white w-full bg-blue-700 hover:bg-blue-800 font-bold text-1xl">LOG IN</button>
                 </form>
-                <p>Belum punya akun? <a href="{{ route('register') }}" class="text-blue-700">Buat</a> Sekarang</p>
+                <p class="mt-2">Belum punya akun? <a href="{{ route('register') }}" class="text-blue-700">Buat</a> Sekarang</p>
             </div>
         </div>
     </div>
-@endsection
-
-@section('addJavascript')
-
 @endsection
--- resources/views/pendaftaran/index.blade.php
+++ resources/views/pendaftaran/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="flex items-center justify-center h-screen">
         <div class="bg-white/30 w-1/3 p-10 rounded-3xl border border-white text-black">
-            <div class="flex justify-end w-100">
-                <a href="{{ route('logout') }}" class="btn border-none p-2 bg-white rounded-xl hover:bg-slate-300">
+            <div class="flex justify-end">
+                <a href="{{ route('logout') }}" class="btn border-none px-3 bg-white rounded-xl hover:bg-slate-300">
                     <img src="{{ asset('images/logout.png') }}" width="20px"/>
                 </a>
             </div>
@@ -13,14 +13,14 @@
             </div>
             <form method="POST" action="{{ route('storePendaftaran') }}">
                 @csrf
-                <label class="mt-8 h-12 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
+                <label class="mt-5 h-12 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
                     <input type="text" class="grow" placeholder="Nomor KK" name="nokk" min="16" required />
                 </label>
                 <label class="mt-3 h-12 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
                     <input type="text" class="grow" placeholder="NIK" name="nik" min="16" required />
                 </label>
                 <label class="mt-3 h-12 rounded-xl input input-bordered flex items-center gap-2" style="background-color: #f1f1f1">
-                    <input type="text" class="grow" placeholder="Nama" name="nama" required />
+                    <input type="text" class="grow" placeholder="Nama Lengkap" name="nama" required />
                 </label>
                 <div class="mt-3 flex items-center">
                     <label class="form-control w-full">
@@ -36,9 +36,9 @@
                 
                 <input type="hidden" name="status" value="aktif" />
                 
-                <button type="submit" class="btn border-none mt-8 p-4 rounded-xl text-white w-full bg-orange-500 hover:bg-orange-600 font-bold text-1xl">Daftar</button>
+                <button type="submit" class="btn border-none mt-5 p-4 rounded-xl text-white w-full bg-orange-500 hover:bg-orange-600 font-bold text-1xl">Daftar</button>
             </form>
-            <a href="{{ route('status') }}" class="btn border-none mt-4 p-4 rounded-xl text-white w-full bg-blue-700 hover:bg-blue-800 font-bold text-1xl">Lihat Status Pendaftaran</a>
+            <a href="{{ route('status') }}" class="btn border-none mt-3 p-4 rounded-xl text-white w-full bg-blue-700 hover:bg-blue-800 font-bold text-1xl">Lihat Status Pendaftaran</a>
         </div>
     </div>
 @endsection
